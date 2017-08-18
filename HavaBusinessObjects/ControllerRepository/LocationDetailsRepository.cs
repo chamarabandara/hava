f64d64f38047484f795ad1bfb2d0a0576678c746
Udayangana Bandara Gajanayaka <udayangana@alliontechnologies.local>
2017-08-18 14:29:50 +0530

    location details for partner

---
+++ HavaBusinessObjects/ControllerRepository/LocationDetailsRepository.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using HavaBusiness;
+using HavaBusinessObjects.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HavaBusinessObjects.ControllerRepository
+{
+    public class LocationDetailsRepository : IDisposable
+    {
+        private HAVA_DBModelEntities context;
+
+        private HAVA_DBModelEntities ObjContext
+        {
+            get
+            {
+                if (context == null)
+                    context = new HAVA_DBModelEntities();
+                return context;
+            }
+        }
+
+        public List<LocationDetailViewModel> GetAllByPartnerId(int id)
+        {
+            var locations = this.ObjContext.LocationDetails
+                .Where(a => a.PartnerId == id).ToList();
+            
+            return Mapper.Map<List<LocationDetail>, List<LocationDetailViewModel>>(locations);
+        }
+
+        #region Dispose
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            this.ObjContext.Dispose();
+        }
+
+        #endregion
+    }
+}
---
+++ WebMVC/Controllers/LocationDetailsController.cs
@@ -0,0 +1,31 @@
+﻿using HavaBusinessObjects.ControllerRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebMVC.Controllers
+{
+    [RoutePrefix("api/locationDetails")]
+    public class LocationDetailsController : ApiController
+    {
+        private LocationDetailsRepository _locationDetailsRepository = new LocationDetailsRepository();
+
+        [HttpGet]
+        [Route("GetAllByPartnerId/{id}", Name = "GetAllByPartnerId")]
+        [AllowAnonymous]
+        public IHttpActionResult GetAllByPartnerId(int id)
+        {
+            try
+            {
+                return Ok(_locationDetailsRepository.GetAllByPartnerId(id));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("General Error");
+            }
+        }
+    }
+}
