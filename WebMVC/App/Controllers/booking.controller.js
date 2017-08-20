3521c47477e23bab95b2d4c4b0fb2234a2cdc791
K.R.Chamara Sandaruwan Bandara <chamara@alliontechnologies.local>
2017-08-20 19:50:54 +0530

    booking changes

---
+++ WebMVC/App/Controllers/booking.controller.js
@@ -0,0 +1,40 @@
+﻿/**
+*   Source File:		            dealer.controller.js
+
+*   Property of Hava All rights reserved.
+
+*   Description:                    Dealer functionalities
+*
+*   Date		    Author/(Reviewer)		Description
+*   -------------------------------------------------------	
+*   17 Aug 2017    Chamara Bandara	        Creation 
+*
+*/
+
+var sitesControllers = angular.module('Sites', []);
+
+sitesControllers.controller('BookingCreateCtrl', ['$scope', '$http', function ($scope, $http) {
+  
+    $scope.search = {};
+    $scope.locations = [{ 'id': 1, 'name': 'test' }];
+    $scope.isMain = true;
+    $scope.searchBooking = function (model) {
+        $scope.isMain = false;
+        $scope.pickupLocation = $('#searchTextField').val();
+    }
+
+    $scope.step = 1;
+    $scope.navigateSteps = function (stp) {
+        $scope.step = stp;
+
+    }
+
+    $scope.whatClassIsIt = function (st) {
+        if (st == $scope.step)
+            return "active"
+        else
+            return "";
+    }
+
+}]);
+
